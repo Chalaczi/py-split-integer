4c7ab72df60a60917752504c57904dc409cb1907
Jakub <jakubmichalski89@fgmail.com>
2025-07-22 13:42:14 +0200

    'Solution'

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -1,6 +1,7 @@
 import pytest
 from app import split_integer
 
+
 @pytest.mark.parametrize(
     "value, number_of_parts",
     [
@@ -12,9 +13,12 @@ from app import split_integer
         (15, 4),
     ],
 )
-def test_sum_of_parts_equals_value(value, number_of_parts):
+def test_sum_of_parts_equals_value(value: int, number_of_parts: int) -> None:
     parts = split_integer.split_integer(value, number_of_parts)
-    assert sum(parts) == value, "Suma elementów powinna równać się wartości wejściowej"
+    assert sum(parts) == value, (
+        "Suma elementów powinna równać się wartości wejściowej"
+    )
+
 
 @pytest.mark.parametrize(
     "value, number_of_parts",
@@ -25,10 +29,11 @@ def test_sum_of_parts_equals_value(value, number_of_parts):
         (15, 4),
     ],
 )
-def test_parts_are_sorted(value, number_of_parts):
+def test_parts_are_sorted(value: int, number_of_parts: int) -> None:
     parts = split_integer.split_integer(value, number_of_parts)
     assert parts == sorted(parts), "Lista powinna być posortowana rosnąco"
 
+
 @pytest.mark.parametrize(
     "value, number_of_parts",
     [
@@ -38,6 +43,10 @@ def test_parts_are_sorted(value, number_of_parts):
         (15, 4),
     ],
 )
-def test_difference_max_min_is_at_most_one(value, number_of_parts):
+def test_difference_max_min_is_at_most_one(
+    value: int, number_of_parts: int
+) -> None:
     parts = split_integer.split_integer(value, number_of_parts)
-    assert max(parts) - min(parts) <= 1, "Różnica między max a min nie powinna przekraczać 1"
+    assert max(parts) - min(parts) <= 1, (
+        "Różnica między max a min nie powinna przekraczać 1"
+    )
